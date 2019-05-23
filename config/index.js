256fcf9465cb7702cb6df8b45b2eaa5acee4a4e6
huoli@qingmutec.com <807364811@qq.com>
2019-05-23 15:03:57 +0800

    fixbug

--- config/index.js
+++ config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/vue/dist',
+    assetsPublicPath: '/qm-vue/dist',
 
     /**
      * Source Maps
